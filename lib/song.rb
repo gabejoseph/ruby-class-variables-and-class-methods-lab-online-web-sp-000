3843775099c90b882ab044dc605bcacb82d5044e
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-20 12:41:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry'
+
+class Song
+  
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count 
+    @@count
+  end
+  
+  def self.artists
+    array = []
+    @@artists.each do |x|
+      if !array.include?(x) 
+        array << x
+      end
+    end
+    array 
+  end 
+  
+  def self.genres
+    array = []
+    @@genres.each do |x|
+      if !array.include?(x) 
+        array << x
+      end
+    end
+    array 
+  end 
+  
+  def self.genre_count
+    hash = {}
+    @@genres.each do |x|
+      binding.pry
+      hash[x] = @name
+    end
+  end
+
+end
